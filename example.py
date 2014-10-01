c1e97c9defd425a4236ccb3dd7f297fb7d241659
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-10-01 02:09:26 -0400

    Add example file for creating a feature vector

---
+++ example.py
@@ -0,0 +1,75 @@
+import re
+
+
+ARYL = ['2', '3', '4', '11', '12']
+ARYL0 = ['2', '3', '11']
+RGROUPS = ['a', 'e', 'f', 'i', 'l']
+
+
+def tokenize(string):
+    '''
+    Tokenizes a given string into the proper name segments. This includes the 
+    addition of '*' tokens for aryl groups that do not support r groups.
+
+    >>> tokenize('4al')
+    ['4', 'a', 'l']
+    >>> tokenize('4al12ff')
+    ['4', 'a', 'l', '12', 'f', 'f']
+    >>> tokenize('3')
+    ['3', '*', '*']
+    >>> tokenize('BAD')
+    ValueError: Bad Substituent Name(s): ['BAD']
+    '''
+    
+    match = '(1?\d|-|[%s])' % ''.join(RGROUPS)
+    tokens = [x for x in re.split(match, string) if x and x != '_']
+
+    valid_tokens = set(ARYL + RGROUPS + ['-'])
+    invalid_idxs = [x for i, x in enumerate(tokens) if x not in valid_tokens]
+    if invalid_idxs:
+        raise ValueError("Bad Substituent Name(s): %s" % str(invalid_idxs))
+
+    new_tokens = []
+    for token in tokens:
+        new_tokens.append(token)
+        if token in ARYL0:
+            new_tokens.extend(['*', '*'])
+    return new_tokens
+
+
+def get_features(name, limit=4):
+    '''
+    Creates a simple boolean feature vector based on whether or not a part is 
+    in the name of the structure.
+
+    >>> get_features('4aa', limit=1)
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+    >>> get_features('3', limit=1)
+    [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    >>> get_features('4aa4aa', limit=1)
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+    >>> get_features('4aa', limit=2)
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0]
+    '''
+    first = ARYL
+    second = ['*'] + RGROUPS
+    length = len(first) + 2 * len(second)
+
+    features = []
+    name = name.replace('-', '')  # no support for flipping yet
+    count = 0
+    for token in tokenize(name):
+        base = second
+        if token in first:
+            if count == limit:
+                break
+            count += 1
+            base = first
+        temp = [0] * len(base)
+        temp[base.index(token)] = 1
+        features.extend(temp)
+
+    # fill features to limit amount of groups
+    features += [0] * length * (limit - count)
+    return features
