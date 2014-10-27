25cfa875ff6e7491aa5bfef4e638cde95f39fcac
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-10-27 01:15:22 -0400

    Add some functions to plot the different feature vectors/properties

---
+++ plot.py
@@ -0,0 +1,55 @@
+import numpy
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from sklearn import decomposition
+
+
+def pca_plot(X, y, title="Principal Component Analysis", save=None):
+    pca = decomposition.PCA(n_components=2)
+    pca.fit(X)
+    temp = pca.explained_variance_ratio_, sum(pca.explained_variance_ratio_)
+    temper = pca.transform(X)
+    Xs = temper[:,0]
+    Ys = temper[:,1]
+    y = numpy.array(y.T.tolist()[0])
+    COLOR = (y-y.min())/y.max()
+    # cm = plt.get_cmap("HOT")
+    plt.scatter(Xs, Ys, c=COLOR, s=15, marker='o', edgecolors='none')
+    plt.title(title + "\n%s %s" % temp)
+    plt.xlabel("PCA 1")
+    plt.ylabel("PCA 2")
+    if save is None:
+        plt.show()
+    else:
+        plt.savefig(save)
+    plt.clf()
+
+
+def plot_and_save_all_pca(features, property_sets):
+    for feat_name, feat in features.items():
+        for prop_name, prop in property_sets:
+            title = feat_name + " " + prop_name
+            save = feat_name + "_" + prop_name + ".png"
+            pca_plot(feat, prop, title=title, save=save)
+
+
+def PCA_stuff_3d(X, y, title="Principal Component Analysis"):
+    pca = decomposition.PCA(n_components=3)
+    pca.fit(X)
+    print pca.explained_variance_ratio_, sum(pca.explained_variance_ratio_)
+    temper = pca.transform(X)
+    Xs = temper[:,0]
+    Ys = temper[:,1]
+    Zs = temper[:,2]
+    y = numpy.array(y.T.tolist()[0])
+    COLOR = (y-y.min())/y.max()
+    cm = plt.get_cmap("HOT")
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(Xs, Ys, Zs, c=COLOR,s=80, marker='o', edgecolors='none')
+    plt.title(title)
+    plt.show()
+    plt.clf()
+
+
