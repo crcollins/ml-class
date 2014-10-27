79c20576c7a8878d620613a8d69550dff4c26f7f
haichenl <haichenl@andrew.cmu.edu>
2014-10-27 17:15:47 -0400

    linear_sine_laplace_rbf kernel svm

--- clfs.py
+++ clfs.py
@@ -28,7 +28,6 @@ def laplace_kernel_gen(sigma):
         return numpy.exp(sigma*-cdist(X,Y))
     return func
 
-
 class SVMLaplace(svm.SVR):
     def __init__(self, kernel='rbf', degree=3, gamma=0.0, coef0=0.0, tol=1e-3,
                  C=1.0, epsilon=0.1, shrinking=True, probability=False,
@@ -39,3 +38,21 @@ class SVMLaplace(svm.SVR):
                  cache_size=cache_size, verbose=verbose, max_iter=max_iter,
                  random_state=random_state)
         self.kernel = laplace_kernel_gen(gamma)
+
+def linear_sine_laplace_rbf_kernel_gen(sine_coef, lap_coef, rbf_coef, omega, lamda, sigma):
+    def func(X, Y):
+        distXY = cdist(X,Y)
+        return numpy.dot(X, Y.T) + sine_coef*numpy.sin(omega*distXY) + lap_coef*numpy.exp(-lamda*distXY) + rbf_coef*numpy.exp( (-distXY**2) / (sigma*sigma) )
+    return func
+
+class SVM_Linear_Sine_Laplace_Rbf(svm.SVR):
+    def __init__(self, kernel='rbf', degree=3, sine_coef=0.0, lap_coef=0.0, rbf_coef=0.0, omega=0.0, lamda=0.0, sigma=0.0, coef0=0.0, tol=1e-3,
+                 C=1.0, epsilon=0.1, shrinking=True, probability=False,
+                 cache_size=200, verbose=False, max_iter=-1,
+                 random_state=None):
+        super(SVM_Linear_Sine_Laplace_Rbf, self).__init__(kernel=kernel, degree=degree, gamma=0.0, coef0=coef0, tol=tol,
+                 C=C, epsilon=epsilon, shrinking=shrinking, probability=probability,
+                 cache_size=cache_size, verbose=verbose, max_iter=max_iter,
+                 random_state=random_state)
+        self.kernel = linear_sine_laplace_rbf_kernel_gen(sine_coef, lap_coef, rbf_coef, omega, lamda, sigma)
+
--- utils.py
+++ utils.py
@@ -160,3 +160,47 @@ def test_clf_kfold(X, y, clf, folds=10):
         test[i] = mean_absolute_error(clf.predict(X_test), y_test)
     return (train.mean(), train.std()), (test.mean(), test.std())
 
+class OptimizedCLF(object):
+    def __init__(self, X, y, func, params):
+        self.params = params
+        self.func = func
+        self.X = X
+        self.y = y
+        self.optimized_clf = None
+        self.optimized_params = None
+        
+    def __call__(self, *args):
+        a = dict(zip(self.params.keys(), *args))
+        clf = self.func(**a)
+        train, test = test_clf_kfold(self.X, self.y, clf, folds=5)
+        return test[0]
+        
+    def get_optimized_clf(self):
+        if not len(self.params.keys()):
+            self.optimized_clf = self.func()
+        if self.optimized_clf is not None:
+            return self.optimized_clf
+        listparams = dict((k,v) for k,v in self.params.items() if type(v) in [list, tuple])
+        itemparams = dict((k,v) for k,v in self.params.items() if type(v) not in [list, tuple])
+        listvalues = []
+        itemvalues = []
+        #~ if listparams:
+            #~ _, test = scan(self.X, self.y, self.func, listparams)
+            #~ listvalues = []
+            #~ temp = numpy.unravel_index(test.argmin(), test.shape)
+            #~ for i, pick in enumerate(listparams.values()):
+                #~ listvalues.append(pick[temp[i]])
+            #~ listvalues = listvalues[::-1]
+        if itemparams:
+            bounds = ((1e-8, None), ) * len(self.params.keys())
+            results = scipy.optimize.fmin_l_bfgs_b(
+                self, self.params.values(),
+                bounds=bounds,
+                approx_grad=True, epsilon=1e-3)
+            itemvalues = results[0].tolist()
+        keys = listparams.keys() + itemparams.keys()
+        values = listvalues + itemvalues
+        self.optimized_params = dict(zip(keys, values))
+        self.optimized_clf = self.func(**self.optimized_params)
+        return self.optimized_clf
+
