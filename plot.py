a1d7ed7bda993fa2f1bb5db493abcfc0607ecb76
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-12-31 14:05:33 -0500

    Remove old colors from plotting code

--- plot.py
+++ plot.py
@@ -248,15 +248,6 @@ def plot_method_errors(values, property_name):
     width = 0.9 / len(errors)
 
     colors = [
-        # "#F15854", # (red)
-        # "#F17CB0", # (pink)
-        # "#FAA43A", # (orange)
-        # "#DECF3F", # (yellow)
-        # "#60BD68", # (green)
-        # "#5DA5DA", # (blue)
-        # "#B276B2", # (purple)
-        # "#B2912F", # (brown)
-        # "#4D4D4D", # (gray)
         "#F44336",
         "#9C27B0",
         "#3F51B5",
